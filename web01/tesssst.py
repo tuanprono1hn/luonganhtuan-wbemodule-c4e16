e6ae0b41106f1d9d36f177ecc680655e1904cbde
tuanprono1hn <a6quocoai@gmail.com>
2018-03-31 23:41:02 +0700

    update

---
+++ web01/tesssst.py
@@ -0,0 +1,15 @@
+users = [
+        {
+        "tuan": {"name" : "Luong Anh Tuan",
+                "gender": 0,
+                "age" : 24,
+                "hobby" : "playing game"}
+        },
+        {
+        "quy" : {"name" : "Dinh Cong Quy",
+                "gender" : 0,
+                "age" : 20,
+                "hobby" : "coding"}
+        }
+        ]
+print(users[0]["tuan"])
